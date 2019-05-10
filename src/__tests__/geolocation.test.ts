665dbd678dedfc3d5fcf85f015131238070357d6
Farzad YZ <farskid@gmail.com>
2019-05-11 00:30:01 +0300

    geolocation support tests

---
+++ src/__tests__/geolocation.test.ts
@@ -0,0 +1,29 @@
+import { getCurrentLocation } from "../geolocation";
+
+describe("geolocation", () => {
+  describe("getCurrentLocation", () => {
+    it("should alert if navigator is not available", () => {
+      (window as any).navigator = undefined;
+      (window as any).alert = jest.fn();
+
+      getCurrentLocation(() => {}, () => {});
+
+      expect((window as any).alert).toHaveBeenCalledTimes(1);
+      expect((window as any).alert).toHaveBeenCalledWith(
+        "This feature is not available in your device"
+      );
+    });
+
+    it("should alert if navigator.geolocation is not available", () => {
+      (window as any).navigator = { geolocation: undefined };
+      (window as any).alert = jest.fn();
+
+      getCurrentLocation(() => {}, () => {});
+
+      expect((window as any).alert).toHaveBeenCalledTimes(1);
+      expect((window as any).alert).toHaveBeenCalledWith(
+        "This feature is not available in your device"
+      );
+    });
+  });
+});
